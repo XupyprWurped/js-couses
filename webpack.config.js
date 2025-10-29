be8f7010e3aaaaf8d730142a412d352169f52f89
XupyprWurped <wurped.sigma@gmail.com>
2025-10-29 16:12:32 +0200

    Added base sturrute

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    clean: true,
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+      watch: true,
+    },
+    watchFiles: ["src/**/*", "data/**/*", "public/**/*"],
+    port: 3000,
+    open: true,
+    compress: true,
+    hot: true,
+    liveReload: true,
+  },
+};
